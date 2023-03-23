626361b688e50520e9039333f6c63189391b5363
Reconfort Daniel <75314029+Reconfort@users.noreply.github.com>
2023-03-23 12:31:33 +0200

    Remove device screen view

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import './dashboard/styles/global.scss'
-import Device from './Components/Device'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Device/>
   </React.StrictMode>,
 )
